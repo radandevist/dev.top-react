3bf1043025fe9c8a1a29d63d06bc4cc2bb138645
Andrianarisoa Daniel <radandevist@gmail.com>
2022-11-20 17:22:32 +0300

    🎉 initiate project with vite and useful libs

---
+++ postcss.config.cjs
@@ -0,0 +1,8 @@
+/** @typedef import('postcss-import'). */
+module.exports = {
+  plugins: {
+    'postcss-import': {},
+    tailwindcss: {},
+    autoprefixer: {},
+  }
+}
---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import "tailwindcss/tailwind.css"
+
+export function App() {
+  return (
+    <>
+      <h1>Welcome to dev.ko</h1>
+      <img src="/vite.svg" alt="vite_logo" />
+    </>
+  )
+}
