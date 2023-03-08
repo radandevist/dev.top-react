3bacbdb0d51904162ea39fcfb1fe14fc514a8dec
Andrianarisoa Daniel <radandevist@gmail.com>
2023-03-08 10:22:43 +0300

    cahnges idk

--- babel-plugin-macros.config.cjs
+++ babel-plugin-macros.config.cjs
@@ -4,20 +4,3 @@ module.exports = {
     preset: 'styled-components',
   },
 }
-
-// module.exports = {
-//   twin: {
-//     styled: {
-//       import: 'default',
-//       from: 'styled-components/macro'
-//     },
-//     css: {
-//       import: 'css',
-//       from: 'styled-components/macro'
-//     },
-//     global: {
-//       import: 'createGlobalStyle',
-//       from: 'styled-components'
-//     }
-//   }
-// }
